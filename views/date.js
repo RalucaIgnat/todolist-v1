f53d05f8cd81f4b7a747d5e04be10977c6daaa38
ralu <ralucaignat56@gmail.com>
2019-11-20 13:10:36 +0200

    First commit

---
+++ views/date.js
@@ -0,0 +1,12 @@
+
+// function getDate() {
+//
+//     let today = new Date();
+//     let options = {
+//         weekday: "long",
+//         day: "numeric",
+//         month: "long"
+//     };
+//     let day = today.toLocaleDateString("en-US", options);
+//         return day;
+//     }
